a05b0779b6fab0535e5b61a7b1f3165733ad7f1c
blazinghorizon <apexkennys@gmail.com>
2020-06-03 02:21:27 +0300

    removed console logs

--- tests/cycles_test.h
+++ tests/cycles_test.h
@@ -11,42 +11,27 @@ extern "C++" {
 
 TEST(mapGeneratorTest, checkForCycles) {
 
-    std::cerr << "[          ] starting cycle" << std::endl;
     for (unsigned int passes = 0; passes < 5; ++passes) {
-        std::cerr << "[          ] pre-generation" << std::endl;
         std::vector<int> map(LWIDTH*LHEIGHT, 0);
         MapGenerator mg;
         mg.levelGenerate(map);
 
-        std::cerr << "[          ] generated map" << std::endl;
         std::vector<std::vector<int>> paths;
         paths = graph_table(map);
-        std::cerr << "[          ] paths vector:" << std::endl;
-        for (unsigned int start = 0; start < LWIDTH*LHEIGHT; ++start)
-        {
-            for (unsigned int start_2 = 0; start_2 < paths[start].size(); ++start_2)
-                std::cerr << paths[start][start_2] << " ";
-            std::cerr << std::endl;
-        }
 
         std::vector<int> flags;
         /* Массив индикаторов захода в выбранную вершину */
         for (unsigned int i = 0; i < LHEIGHT*LWIDTH; ++i) {
             flags.push_back(0);
         }
-        std::cerr << "[          ] dfs started" << std::endl;
+
         /* Вызываем поиск цикла, если он найден, отмечаем*/
         bool cycle = false;
         if (dfs(0, -1, paths, flags)) {
             cycle = true;
         }
 
-        std::cerr << "[          ] cycle detected: " << cycle << std::endl;
-        std::cerr << "[          ] for: " << passes + 1 << "time" << std::endl;
-
         ASSERT_EQ(cycle, true);
-
-        std::cerr << "[          ] vectors cleared" << std::endl;
     }
 }
 #endif // CYCLES_CHECK_H
