1eaaaad0d5ba0f99263ba44b5a92d8a7ea7230db
Kalyushin <60959640+Kalyushin@users.noreply.github.com>
2020-06-03 02:56:35 +0300

    Update load_test.h

--- tests/load_test.h
+++ tests/load_test.h
@@ -7,12 +7,17 @@
 extern "C++" {
 #include "defines.h"
 #include "map.h"
-#include "hero.h"
-#include "enemy.h"
+}
+
+Map createMap (){
+    Map testMap;
+    return testMap;
 }
 
 TEST(loadTestNegative, nonexistentFile) {
-    Map map;
+    std::vector<int> level(LHEIGHT*LWIDTH, 0);
+    ASSERT_EQ(0, 0);
+    createMap().load("nonexistentFile.png",sf::Vector2u(128, 128), testlevel, LWIDTH, LHEIGHT);
     ASSERT_EQ(1, 0);
 }
 
