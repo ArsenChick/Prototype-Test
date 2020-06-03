1f17f1a27851a177a16c5b345401a3470d4a1d71
Kalyushin <60959640+Kalyushin@users.noreply.github.com>
2020-06-03 04:09:47 +0300

    Update load_test.h

--- tests/load_test.h
+++ tests/load_test.h
@@ -3,7 +3,11 @@
 
 #include <gtest/gtest.h>
 #include "test_assist.h"
-
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <cstdio>
 extern "C++" {
 #include "defines.h"
 #include "map.h"
@@ -12,8 +16,41 @@ extern "C++" {
 TEST(loadTestNegative, nonexistentFile) {
     std::vector<int> testlevel(LHEIGHT*LWIDTH, 0);
     Map testMap;
+    
+    сhar *debug = (char *)malloc(sizeof(char)*64);
+    snprintf(debug, 64, "file.log");
+    
+    int newSTDerr = open(debug, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    ASSERT_NE(newSTDerr, -1);
+    int oldSTDerr = dup(STDERR_FILENO);
+    close(STDERR_FILENO);
+    dup2(newSTDerr, STDERR_FILENO);
+    
     bool value = testMap.load("nonexistentFile.png",sf::Vector2u(128, 128), testlevel, LWIDTH, LHEIGHT);
     ASSERT_EQ(value, false);
+    
+    fflush(stderr);
+    close(newSTDerr);
+    dup2(oldSTDerr, STDERR_FILENO);
+
+    int testFD = open(debug, O_RDONLY);
+    
+    char *testBuf = (char *)malloc(sizeof(char)*64);
+    char *outBuf = (char *)malloc(sizeof(char)*64);
+    int testCount;
+
+    testCount = read(testFD, testBuf, 64);
+    snprintf(outBuf, 64, "Failed to load image \"nonexistentFile.png\"\n");
+    ASSERT_TRUE(testCount > 0);
+    close(testFD);
+
+    for(int i = 0; i < testCount; i++)
+        ASSERT_EQ(outBuf[i], testBuf[i]);
+
+    free(outBuf);
+    free(testBuf);
+    
+    free(debug);
 }
 
 #endif // LOAD_TEST_H
